ef242783260c960150a72a29adf8627d27b2e367
Pierre Lambert <supagoof924@hotmail.com>
2019-06-05 22:50:09 +0200

    [ REFACTO ] Updated ResourceList to no longer use class based syntax or methods

--- src/components/ResourceList.js
+++ src/components/ResourceList.js
@@ -1,17 +1,21 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
-const ResourceList = () => {
-  const [resource, setResource] = useState([]);
+const ResourceList = ({ resource }) => {
+  const [resources, setResources] = useState([]);
 
-  /* async componentDidMount() {
+  const fetchResource = async resource => {
     const response = await axios.get(
-      `https://jsonplaceholder.typicode.com/${this.props.resource}`
+      `https://jsonplaceholder.typicode.com/${resource}`
     );
-    this.setState({ resources: response.data });
-  } */
+    setResources(response.data);
+  };
 
-  return <div>{resource.length}</div>;
+  useEffect(() => {
+    fetchResource(resource);
+  }, [resource]);
+
+  return <div>{resources.length}</div>;
 };
 
 export default ResourceList;
